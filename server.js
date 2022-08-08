c74cf5c60f160019b3a2133af86c43e67dfb60ed
janae.wel <janae.wel@gmail.com>
2022-08-08 14:31:47 -0400

    fixing endpoint err

--- server.js
+++ server.js
@@ -18,28 +18,50 @@ const db = mysql.createConnection(
     console.log('Connected to the election database')
 );
 
-// queries the db var and should return all of the rows
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// get all candidates
+app.get('/api/candidates', (req, res) => {
+    // queries the db var and returns all of the rows
+    db.query(`SELECT * FROM candidates`, (err, rows) => {
+        if (err){
+            // REMEMBER 500 indicates a server error
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({message: 'success', data: rows});
+    });
 });
 
-// to select a single row
-db.query(`SELECT * FROM candidates WHERE id= 1`, (err, row) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(row);
+// get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({message: 'success', data: row});
+    });
 });
 
-// Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-    VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
+// Delete a candiate
+app.get('api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({error: res.message});
+            // affectedRows will return the affected rows
+        } else if (!result.affectedRows) {
+            res.json({message: 'Candidate not found'});
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
 });
 
 // Default for any other requests (Not Found)
@@ -53,6 +75,25 @@ app.listen(PORT, () => {
 
 // to delete a single row
 // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// to select a single row
+// db.query(`SELECT * FROM candidates WHERE id= 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// db.query(sql, params, (err, result) => {
 //     if (err) {
 //         console.log(err);
 //     }
