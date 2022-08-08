b85d7aa7bbe4cbe5f76dbab3a1c0d88e92782878
janae.wel <janae.wel@gmail.com>
2022-08-07 20:48:25 -0400

    established connection to express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Express middleware
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// Default for any other requests (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+});
